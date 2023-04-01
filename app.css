6ad6616121314e2a9a643438fc2855e47c19d272
Efrain Yaphet Obert <efrainzhenwu@gmail.com>
2023-04-01 14:44:54 +0700

    masalah pada media css

---
+++ app.css
@@ -0,0 +1,33 @@
+body{
+    background-color: #c2ded1;
+    font-family: 'Nutino', sans-serif;
+}
+
+#navbar {
+    font-size: 1.5em;
+    font-weight: 100;
+}
+
+#navbar .nav-link{
+    color: white;
+}
+
+#navbar .nav-link:hover{
+    color: #354269;
+}
+#navbar .nav-brand{
+    color: #354269;
+    font-size: 1.5rem;
+}
+
+#title-head{
+    font-weight: 100;
+    font-size: 4rem;
+}
+
+@media (max-width: 1200px){
+    #title-head h1 {
+        font-weight: 100;
+        font-size: 3rem;
+    }
+}
